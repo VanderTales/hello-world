619cbed84dc3d6fa9dce750f7cc8e15585b2dab3
Vander Tales <vander.caparroz@aluno.unip.br>
2022-02-21 05:01:31 -0300

    primeiros commit, primeiras aulas do curso de JS e primeiro do curso sobre o Git

---
+++ JScourse/inicio/comentario.js
@@ -0,0 +1,7 @@
+//console.log é usado para exibir algo na tela do computador!
+console.log('passo #01');
+//console.log('passo #03');
+console.log('passo #02');
+/*
+use comentarios relevantes
+*/
---
+++ JScourse/inicio/sentenca_2.js
@@ -0,0 +1,4 @@
+//teste comentario
+console.log('passo #01');
+console.log('passo #02');
+console.log('passo #03');
