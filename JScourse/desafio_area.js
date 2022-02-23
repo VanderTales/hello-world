ed5b01811148abf132b0d397d6b971fe8a527b0a
Vander Tales <vander.caparroz@aluno.unip.br>
2022-02-23 04:58:59 -0300

    Os ultimos arquivos da parte 1 do curso de JS

---
+++ JScourse/desafio_area.js
@@ -0,0 +1,11 @@
+// PI * raio * raio
+
+const raio = 10;
+
+const PI = 3.1416;
+
+let area = PI * raio * raio;
+
+console.log("O valor da área é: " + area + "m2.");
+
+area = Math.PI * raio * raio;
---
+++ JScourse/desafio_troca.js
@@ -0,0 +1,16 @@
+let  a = 7
+let b = 94
+
+console.log("a é: "+ a)
+console.log("b é: "+ b)
+
+let troca = 0
+
+troca = a
+a = b
+b = troca
+
+console.log("a é: "+ a)
+console.log("b é: "+ b)
+
+
