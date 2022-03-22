e015dfa4fdd1d3c5cc6931cff0a8f4a59068c49f
Vander Tales <vander.caparroz@aluno.unip.br>
2022-03-21 21:49:57 -0300

    aulas sobre this/bind e arrow function

---
+++ JScourse/arrowFunction3.js
@@ -0,0 +1,14 @@
+comparaComThis = function (param)
+{
+    console.log(this === param)
+}
+comparaComThis(global)
+
+const obj = {}
+comparaComThis = comparaComThis.bind(obj)
+comparaComThis(global)
+comparaComThis(obj)
+
+let comparaComThisArrow = param => console.log(this === param)
+comparaComThisArrow(global)
+comparaComThisArrow(module.exports)
